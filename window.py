2d2d994a8fe44e9cc7877fda8bdda42c47e12f48
Julos <julesbrn85@gmail.com>
2022-02-23 18:25:54 +0100

    Ajout de la première question avant fonctionement du bouton vérification

--- window.py
+++ window.py
@@ -31,9 +31,10 @@ win.config(background=bg)
 # texte
 txt_1 = tk.Label(frame1, text="Learn HTML !", font=(font, 35), bg=bg)
 txt_2 = tk.Label(frame1, text="avec notre superbe application !", font=(font, 20), bg=bg)
+txt_3 = tk.Label(frame1, text="Cette application est bien.", font=(font, 15), bg=bg)
 
 # bouton
-btn = tk.Button(frame1, text="vérification", font=(font, 15))
+btn = tk.Button(frame1, text="Vérification", font=(font, 15))
 
 # cases à cochers
 check = tk.Checkbutton(frame1, text="Vrai", font=(font, 25), variable=test1, bg=bg)
@@ -42,6 +43,7 @@ check2 = tk.Checkbutton(frame1, text="Faux", font=(font, 25), variable=test2, bg
 # pack()
 txt_1.pack()
 txt_2.pack()
+txt_3.pack(pady=(20, 0))
 check.pack()
 check2.pack()
 btn.pack(pady=15, fill=tk.X)
